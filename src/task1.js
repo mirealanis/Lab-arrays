d232a92a8c1c9d30e2a894f0e8888344229d2328
Mireya Alanis <alanisgmireya@gmail.com>
2026-09-01 12:40:29 -0600

    feat: agregar soluciones a tareas 1, 2, 3 y 4

--- src/task1.js
+++ src/task1.js
@@ -1,9 +1,10 @@
 const arr = []
 
 // Escribe tu codigo por debajo de esta linea
-
-
-
+for (let i = 0; i < 20; i++) {
+  arr.push(i + 1);
+  //console.log(`${arr[i]}`);
+}
 // Escribe tu codigo por encima de esta linea
 
 //No tocar esta funcion
--- src/task3.js
+++ src/task3.js
@@ -1,8 +1,9 @@
 const arr = [1,2,3,4,5,6,7,9,10,11,12]
 
 // Escribe tu codigo por debajo de esta linea
-
-
+//console.log(arr);
+arr.splice(7,0,8);
+//console.log(arr);
 
 // TEscribe tu codigo por encima de esta linea
 
